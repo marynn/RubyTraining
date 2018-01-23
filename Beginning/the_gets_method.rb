4a813acd0d89a5af84ca997886f0c0e30a1c533d
mnosulko <mnosulko@gmail.com>
2018-01-23 16:22:18 +0000

    Made some corrections in the tasks and lectures.

--- Beginning/the_gets_method.rb
+++ Beginning/the_gets_method.rb
@@ -1,4 +1,5 @@
 # gets method returns a String
+
 puts "Hi, what's your name?"
 name = gets.chomp # chomp method removes the \n at the end of the String
 
@@ -6,4 +7,3 @@ puts "Great! What's your age?"
 age = gets.chomp.to_i
 
 puts "Cool, so your name is #{name} and you are #{age} years old."
-
