bc9fac1f1c655e6a3f05a72124f4131315e9da11
mnosulko <mnosulko@gmail.com>
2018-01-27 22:55:26 +0000

    Made some corrections in the tasks.

--- Exercises/calculator_using_case_statement_2.rb
+++ Exercises/calculator_using_case_statement_2.rb
@@ -26,7 +26,7 @@ puts "Welcome to the calculator, what would you like to do?\n1)multiply, 2)divid
 prompt = gets.chomp
 
 while prompt.to_i < 1 || prompt.to_i > 5
-  puts 'Wrong number, enter the operation number again'
+  puts 'Wrong number, please enter the valid operation number'
   prompt = gets.chomp
 end
 
@@ -35,20 +35,20 @@ first_number = gets.chomp
 puts 'What do you want the second number to be?'
 second_number = gets.chomp
 
-case prompt
-  when '1' then
+case prompt.to_i
+  when 1 then
     puts "You have chosen to multiply #{first_number} by #{second_number}"
     puts "The first number multiplied by the second number is: #{multiply(first_number, second_number)}"
-  when '2' then
+  when 2 then
     puts "You have chosen to divide #{first_number} by #{second_number}"
     puts "First number divided by second number is: #{divide(first_number, second_number)}"
-  when '3' then
+  when 3 then
     puts "You have chosen to add #{first_number} to #{second_number}"
     puts "The first number added to the second number is: #{sum(first_number, second_number)}"
-  when '4' then
+  when 4 then
     puts "You have chosen to subtract #{second_number} from #{first_number}"
     puts "Second number subtracted from the first number is: #{subtract(first_number, second_number)}"
   else
     puts "You have chosen to calculate a modulo of #{second_number} from #{first_number}"
-    puts "First number modulo the second number is: #{modulo(first_number, second_number)}"
+    puts "#{first_number} modulo #{second_number} is: #{modulo(first_number, second_number)}"
 end
